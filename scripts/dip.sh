aa6e3129b662d41940d311bce4570cb135eb93e3
Sarven Capadisli <info@csarven.ca>
2011-08-08 11:59:53 +0100

    Updated to work with multiple dataDumps mentioned in VoID,\nand using dataDump URL for the graph name.

--- scripts/dip.sh
+++ scripts/dip.sh
@@ -1,59 +1,62 @@
 #!/bin/bash 
 #SMCS: Sarven's Magical Collection of Scripts for DIP: Data Ingestion Pipeline
-#Usage: dip.sh voidURL e.g., dip.sh http://example.org/void.ttl
+#Usage: dip.sh voidurl e.g., dip.sh http://example.org/void.ttl
 #Make sure that uncompress.sh is in the same directory as this script.
 
 #Config
 #Location to store the dumps
-BASEDIR='/var/www/datadumps'
-#Dataset name that is used in Fuseki where we import our data
-DATASET='dataset'
-#Port number in which we are running the Fuseki server
-PORT='3030'
+basedir='/var/www/data-gov.ie/data'
+#dataset name that is used in Fuseki where we import our data
+dataset='dataset'
+#port number in which we are running the Fuseki server
+port='3131'
 
 if [ $# -eq 0 -o "$1" = "-h" -o "$1" = "--h" -o "$1" = "-help" -o "$1" = "--help" ]
    then
-       echo "Usage: dip.sh voidURL"
+       echo "Usage: dip.sh voidurl"
        echo "       example: dip.sh http://example.org/void.ttl"
        exit 1
 else
-   VOIDURL=$1
+   voidurl=$1
 fi
 
-VOIDDIR=`echo -n $VOIDURL | perl -pe 's/[^a-zA-Z0-9\._-]/_/g'`
-VOIDDIR="$BASEDIR/$VOIDDIR"
+voidurlsafe=`echo -n "$voidurl" | perl -pe 's/[^a-zA-Z0-9\._-]/_/g'`
+voiddir="$basedir/$voidurlsafe"
 
-mkdir $VOIDDIR
-VOIDFILE="$VOIDDIR/void.ntriples"
+mkdir "$voiddir"
+voidfile="$voiddir/void.ntriples"
 
-rapper -g $VOIDURL -o ntriples > $VOIDFILE
+rapper -g "$voidurl" -o ntriples > "$voidfile"
 
 #XXX: Ideally we should see what the bnode has to say for the dataDump, but, this should suffice. No need to have a bnode there any way.
-DATADUMPURL=`grep -E "[^ ]* <http://rdfs.org/ns/void#dataDump> [^ ]* ." $VOIDFILE | perl -pe 's/([^ ]*) \<http\:\/\/rdfs\.org\/ns\/void\#dataDump\> \<?([^ >]*)\>? \./$2/'`
+ddu=`grep -E "[^ ]* <http://rdfs.org/ns/void#dataDump> [^ ]* ." $voidfile | perl -pe 's/([^ ]*) \<http\:\/\/rdfs\.org\/ns\/void\#dataDump\> \<?([^ >]*)\>? \./$2/'`
 
-#TODO: Iterate (wget) through all dataDump URLs here
+#datadumpurlMD5=`echo -n $datadumpurl|md5sum|cut -f1 -d" "`
 
-#DATADUMPURLMD5=`echo -n $DATADUMPURL|md5sum|cut -f1 -d" "`
-DATADUMPFILESAFE=`echo -n $DATADUMPURL | perl -pe 's/[^a-zA-Z0-9\.-]/_/g'`
-
-DATADUMPFILE="$VOIDDIR/$DATADUMPFILESAFE"
-
-
-wget -O - $DATADUMPURL > $DATADUMPFILE
-#wget $DATADUMPURL --directory-prefix=$TARGETDIR
-
-FILETYPE=`file $DATADUMPFILE`
-echo $FILETYPE
-case $FILETYPE in
-    "$DATADUMPFILE: gzip compressed"* | "$DATADUMPFILE: bzip2 compressed"* | "$DATADUMPFILE: compress"* | "$DATADUMPFILE: Zip archive"* | "$DATADUMPFILE: 7-zip archive"* | "$DATADUMPFILE: RAR archive"*)
-        ./uncompress.sh $DATADUMPFILE $VOIDDIR
-        ;;
-    *)
-        echo "dataDump is not compressed. We should have our RDF triples already."
-        ;;
-esac
-
-for file in $VOIDDIR/*.ttl;
+for datadumpurl in $( echo -e "$ddu" );
     do
-        /usr/lib/fuseki/./s-post --verbose http://localhost:$PORT/$DATASET/data $DATADUMPURL $file;
+        echo $datadumpurl
+        datadumpfilesafe=`echo -n "$datadumpurl" | perl -pe 's/[^a-zA-Z0-9\.-]/_/g'`
+
+        datadumpfile="$voiddir/$datadumpfilesafe"
+
+        wget -O - "$datadumpurl" > "$datadumpfile"
+        #wget $datadumpurl --directory-prefix=$TARGETDIR
+
+        filetype=`file "$datadumpfile"`
+        case "$filetype" in
+            "$datadumpfile: gzip compressed"* | "$datadumpfile: bzip2 compressed"* | "$datadumpfile: compress"* | "$datadumpfile: Zip archive"* | "$datadumpfile: 7-zip archive"* | "$datadumpfile: RAR archive"*)
+                ./uncompress.sh "$datadumpfile" "$voiddir"
+                ;;
+            *)
+                echo "dataDump is not compressed. We should have our RDF triples already."
+                ;;
+        esac
+    done
+
+
+for file in "$voiddir"/*.ttl;
+    do
+        /usr/lib/fuseki/./s-post --verbose http://localhost:"$port"/"$dataset"/data "$voidurl" "$file";
     done;
+
