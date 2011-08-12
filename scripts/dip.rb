ff92fa1cefaa9de3a0b6ca224db867340a2493c7
Sarven Capadisli <info@csarven.ca>
2011-08-12 13:34:13 +0100

    The script creates a directory for each dataDump; grabs the graphName for a dataDump when available; a bunch of other stuff.

--- scripts/dip.rb
+++ scripts/dip.rb
@@ -43,29 +43,86 @@ end
 
 
 def handleFileType(datadumpfile)
-    fm = FileMagic.new
+    fileContents = datadumpfile
 
+    fm = FileMagic.new
     filetype = fm.file(datadumpfile)
+
+    target = datadumpfile + "-x"
+
+    puts "Target: " + target
+    %x[mkdir #{target}]
 
     case filetype;
         when /gzip compressed.*/
-            puts %x[tar zxvf #{datadumpfile} -C #{$voiddir} --overwrite]
+            puts %x[tar zxvf #{datadumpfile} -C #{target} --overwrite]
+            fileContents = %x[tar --list --file=#{datadumpfile}]
         when /POSIX tar.*/
-            puts %x[tar xvf #{datadumpfile} -C #{$voiddir} --overwrite]
+            puts %x[tar xvf #{datadumpfile} -C #{target} --overwrite]
+            fileContents = %x[tar --list --file=#{datadumpfile}]
         when /bzip2 compressed.*/
-            puts %x[tar jxvf #{datadumpfile} -C #{$voiddir} --overwrite]
+            puts %x[tar jxvf #{datadumpfile} -C #{target} --overwrite]
         when /Zip archive.*/
-            puts %x[unzip -o #{datadumpfile} -d #{$voiddir}]
+            puts %x[unzip -o #{datadumpfile} -d #{target}]
         when /7-zip archive.*/
-            puts %x[7za x -y #{datadumpfile} -o#{$voiddir}]
+            puts %x[7za x -y #{datadumpfile} -o#{target}]
         when /RAR archive.*/
-            puts %x[rar -o+ x #{datadumpfile} #{$voiddir}]
+            puts %x[rar -o+ x #{datadumpfile} #{target}]
 
         else
             puts "XXX: dataDump is not compressed. This is okay for now. We'll check later to really make sure it is one of the RDF formats"
     end
 
     fm.close
+
+    return fileContents
+end
+
+def getTriples(index, subjects = nil, properties = nil, objects = nil)
+    triples = {}
+
+    if (!subjects.nil? && !subjects.kind_of?(Array))
+        subjects = [subjects]
+    end
+
+    if (!properties.nil? && !properties.kind_of?(Array))
+        properties = [properties]
+    end
+
+    if (!objects.nil? && !objects.kind_of?(Array))
+        objects = [objects]
+    end
+
+    index.each do |s, po|
+        s_candidate = nil;
+
+        if (subjects.nil? || subjects.include?(s))
+            s_candidate = s;
+
+            po.each do |p, o|
+                p_candidate = nil;
+
+                if (properties.nil? || properties.include?(p))
+                    p_candidate = p;
+
+                    o.each do |o_key|
+                        o_candidate = nil
+
+                        if (objects.nil? || objects.include?(o_key))
+                            o_candidate = o_key;
+
+
+                            triples[s_candidate] ||= {}
+                            triples[s_candidate][p_candidate] ||= []
+                            triples[s_candidate][p_candidate] << o_candidate
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    return triples
 end
 
 
@@ -77,7 +134,6 @@ end
 
 $voidurl = ARGV[0]
 $voidurlsafe = $voidurl.gsub(/[^a-zA-Z0-9\._-]/, '_')
-#puts voidurlsafe
 
 $voiddir=$basedir + $ds + $voidurlsafe
 
@@ -88,35 +144,86 @@ end
 puts "Directory " + $voiddir + " already exists." + $nl
 
 $voidfile=$voiddir + $ds + "void.nt"
-#puts voidfile
+puts voidfile
 
 puts "Attempting to get " + $voidurl + " and copy over to " + $voidfile + $nl
 %x[rapper -g #{$voidurl} -o ntriples > #{$voidfile}]
 
-puts "Grepping " + $voidfile + $nl
+puts "Analyzing " + $voidfile + $nl
 
-ddu = Array.new
+ddd = {}
+triples = {}
+
 file = File.new($voidfile, "r")
 while (line = file.gets)
-    if /[^ ]* <http:\/\/rdfs\.org\/ns\/void#dataDump> [^ ]* \./ =~ line
-        ddu.push(line.gsub(/([^ ]*) \<http:\/\/rdfs\.org\/ns\/void#dataDump\> \<?([^ >]*)\>? \./, '\2'))
+    l = Array.new(line.split(/([^ ]*) ([^ ]*) ([^\>]*[\>]?) (\.)(.*)/))
+    s = l[1]
+    p = l[2]
+    o = l[3]
+
+    triples[s] ||= {} # Create a sub-hash unless it already exists
+    triples[s][p] ||= []
+    triples[s][p] << o
+end
+
+dataDumps = getTriples(triples, nil, "<http://rdfs.org/ns/void#dataDump>", nil)
+
+if dataDumps.length > 0
+    dataDumps.each do |a, b|
+        datadumpurl = nil
+        b.each do |x, y|
+            datadumpurl = y
+            ddd[datadumpurl] ||= {}
+        end
+
+        sdGraphs = getTriples(triples, nil, "<http://www.w3.org/ns/sparql-service-description#graph>", a)
+
+        if sdGraphs.length > 0
+            sdGraphs.each do |c, d|
+                sdNames = getTriples(triples, c, "<http://www.w3.org/ns/sparql-service-description#name>", nil)
+                if sdNames.length > 0
+                    sdNames.each do |x, y|
+                        y.each do |w, u|
+                            ddd[datadumpurl][u] ||= []
+                        end
+                    end
+                end
+
+            end
+        end
     end
 end
-file.close
 
+if ddd.length > 0
+    ddd.each do |i, j|
+        i.each do |ddu, b|
+            ddu.gsub!(/[\<\>]/, '')
+            datadumpfilesafe = ddu.gsub(/[^a-zA-Z0-9\._-]/, '_')
 
-ddu.each do |datadumpurl|
-    datadumpurl.strip!
-    datadumpfilesafe = datadumpurl.gsub(/[^a-zA-Z0-9\._-]/, '_')
+            datadumpfile = $voiddir + $ds + datadumpfilesafe
+            datadumpfile.strip!
 
-    datadumpfile = $voiddir + $ds + datadumpfilesafe
-    datadumpfile.strip!
+            response = getURL(ddu)
+            puts "Copying " + ddu + " to " + datadumpfile
+            file = File.new(datadumpfile, "w+")
+            file.write(response.body)
+            file.close
 
-    response = getURL(datadumpurl)
-    puts "Copying " + datadumpurl + " to " + datadumpfile
-    file = File.new(datadumpfile, "w+")
-    file.write(response.body)
-    file.close
+            fileContents = handleFileType(datadumpfile)
 
-    handleFileType(datadumpfile)
+            target = datadumpfile + "-x"
+            FileUtils.mv(datadumpfile, target)
+            puts "File moved"
+
+            graphName = $voidurl
+            puts "Current graphName: " + graphName
+            #For graph names
+            if j.length > 0
+                j.each do |x, y|
+                    graphName = x[0].gsub(/[\<\>]/, '')
+                    puts "New graphName: " + graphName
+                end
+            end
+        end
+    end
 end
